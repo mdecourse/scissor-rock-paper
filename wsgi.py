b63cac8735ba48990c7da13ed1702b9e316c5d3d
mdecourse <mdecourse@gmail.com>
2020-12-21 16:03:27 +0800

    use port 8080

--- wsgi.py
+++ wsgi.py
@@ -18,5 +18,6 @@ app.register_blueprint(game_routes)
 
 #app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=context)
 # for Heroku deployment
-app.run(host="127.0.0.1", port="5000", debug=True)
 
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
