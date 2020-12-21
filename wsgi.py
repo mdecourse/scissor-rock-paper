92c2ab03fa10808224b865fc26a60ca1dc31d616
mdecourse <mdecourse@gmail.com>
2020-12-21 15:49:33 +0800

    comment out ssl setup for Heroku deployment

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,12 @@
 
 import os
 from flask import Flask
-import ssl
+# for Heroku deployment
+#import ssl
 
 # even on localhost still use https to access
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain('localhost.crt', 'localhost.key')
+#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+#context.load_cert_chain('localhost.crt', 'localhost.key')
 
 from routes import game_routes
 
@@ -15,5 +16,7 @@ app = Flask(__name__)
 app.secret_key = 'A0Zr9@8j/3yX R~XHH!jmN]LWX/,?R@T'
 app.register_blueprint(game_routes)
 
-app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=context)
+#app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=context)
+# for Heroku deployment
+app.run(host="127.0.0.1", port="5000", debug=True)
 
